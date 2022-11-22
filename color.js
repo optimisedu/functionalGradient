3401f682bb325a98243b52782f0b15147781f224
optimiseu <optimisedu@gmail.com>
2022-11-22 18:13:48 +0000

    bgcol

--- color.js
+++ color.js
@@ -10,7 +10,8 @@ const btn__random = document.getElementById('btn__random');
 const box__one = document.getElementById('btn__select');
 const box__two = document.getElementById('btn__random');
 const bgCol = document.body.style.backgroundColor;
-
+const btn = document.getElementsByClassName('btn');
+const pngToJpg = document.getElementById('png');
 //utilities
 let random = (min, max) => {
 	return Math.floor(Math.random() * (max - min + 1) + min);
@@ -29,20 +30,30 @@ let set = (selector, property, value) => {
 
 //functions
 let randomColor = () => {
-	let r = random(0, 126);
-	let g = random(0, 255);
+	let r = random(0, 255);
+	let g = random(0, 0);
 	let b = random(0, 255);
 	let a = random(0.5, 1);
 	let color = `rgba(${r}, ${g}, ${b}, ${a})`;
 	return color;
 };
+
 set('main', 'background-color', randomColor());
 btn__random.addEventListener('click', (e) => {
-	set('main', 'background-color', randomColor());
+set('main', 'background-color', randomColor());
 	onerror = (e) => {
 		console.log(e);
 	};
 });
 
-//==============================================get color values==============================================
+// ==============================================get color values==============================================
+let getColor = (el, property) => 
+let gradient = (el1, el2) => {
+	let color1 = get(el1, 'background-color');
+	let color2 = get(el2, 'background-color');
+	let gradient = `linear-gradient(${color1}, ${color2})`;
+	return gradient;
+}
+// ==============================================sharp png to jpg==============================================
+
 
