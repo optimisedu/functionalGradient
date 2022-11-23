5b3d354104fc218abb9fe3a8209c2324e35298d5
optimiseu <optimisedu@gmail.com>
2022-11-23 13:48:47 +0000

    basic functionality added edge case testing req

--- color.js
+++ color.js
@@ -1,25 +1,27 @@
 //get all elements including the css values which can be modified from rt css variables
 // import './styles.css';
-let primary = '--color-primary';
-let secondary = '--color-secondary';
+// let primary = '--color-primary';
+// let secondary = '--color-secondary';
 let png = document.getElementById('png');
 let rt = document.querySelector(':root');
 let int = document.querySelector('#int');
 let out = document.getElementById('out');
 let btn__select = document.getElementById('btn__select');
 let btn__random = document.getElementById('btn__random');
-let box__one = document.getElementById('btn__select');
-let box__two = document.getElementById('btn__random');
+let box__one = document.getElementById('box__one');
+let box__two = document.getElementById('box__two');
 let bgCol = document.body.style.backgroundColor;
 let btn = document.getElementsByClassName('btn');
 let pngToJpg = document.getElementById('png');
 let btn__set = document.getElementById('set');
 //utilities
+let randomInt = (n) =>{ Math.floor(Math.random() * n) += 1};
+
 let random = (min, max) => {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 };
-let randomInt = (n) =>{ Math.floor(Math.random() * n) += 1};
 
+//================================================get and set functions===================================
 
 let el = (selector) => document.getElementById(id);
 let find = (selector) => document.querySelector(selector);
@@ -40,16 +42,32 @@ let set = (selector, property, value) => {
 // 	return color;
 // };
 
-function randomColor() {
+let randomColor = () => {
 	return `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
-  }
-  function intVal(int){
-	return int.value;
-  }
-//   int.value = randomColor();
-//   int.addEventListener('onchange', int);
+  };
 
 set('main', 'background-color', randomColor());
+function setStyle(box, val) {
+	elm.style.color = val;
+  };
+  
+
+//==================================================listeners==================================================
+
+box__one.addEventListener('change', function(){
+	let val = this.value;
+	set('main', 'background-color', val);
+	console.log(val);
+	out.value = this.value;
+});
+
+box__two.addEventListener('change', function(){
+	let val = this.value;
+	set('main', 'background-color', val);
+	console.log(val);
+	out.value = this.value;
+});
+
 btn__random.addEventListener('click', (e) => {
 let thisrand = randomColor();
 	set('main', 'background-color', randomColor());
@@ -60,11 +78,11 @@ let thisrand = randomColor();
 	};
 });
 
-btn__select.addEventListener('click', (e) => {
+btn__select.addEventListener('click', () => {
 	let thisint =  '#' + int.value.toString().slice(-6);
 	console.log(thisint);
 	set('main', 'background-color', thisint);
-	out.value = int.value;
+	out.value = thisint;
 	onerror = (e) => {
 		console.log(e);
 	};
